ada8dc8aaaa409b47cb940161d30165e2b5e4b8c
nguyenhieuxyz159@gmail.com <tompdo2002@gmail.com>
2023-10-12 01:39:02 +0700

    2/7 function

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
-import pymongo
+import time
+from colorama import Fore
 
 
 def create_socket(h, p):
@@ -41,11 +42,103 @@ def start_client(sk):
 
 
 def make_request(sk):
+    login(sk)
     while True:
+        print(Fore.CYAN + '(All(1) Board(2) Search(3) Sell(4) Buy(5) History(6) Exit(7)' + Fore.RESET)
         req = input('Add request here: ')
-        send_req(sk, req)
+        menu(sk, req)
+        # send_req(sk, req)
         if req == 'Exit':
             break
+    return
+
+
+# def process_server_response(sk):
+#     res = recv_res(sk)
+#     if res == 'success':
+#         return ''
+#     if res == '0':
+#         return 'fail'
+
+
+def login(sk):
+    username = input('Username: ')
+    password = input('Password: ')
+    client_name = username + ' ' + password
+    send_req(sk, client_name)
+
+    res = recv_res(sk)
+    if res == 'success':
+        return
+    if res == 'fail':
+        print('Username or Password incorrect !!!')
+        login(sk)
+
+
+def menu(sk, req):
+    if req == '1':
+        all_item(sk)
+
+    if req == '2':
+        top_server(sk)
+
+    # if req == '3':
+    #     search_item(sk)
+    #
+    # if req == '4':
+    #     buy_item(sk)
+    #
+    # if req == '5':
+    #     sell_item(sk)
+    #
+    # if req == '6':
+    #     transaction_history(sk)
+    #
+    # if req == '7':
+    #     disconnect(sk)
+
+
+def all_item(sk):
+    print(Fore.CYAN + 'ALL STOCKS AVAILABLE: ' + Fore.RESET)
+    send_req(sk, '1')
+    res = recv_res(sk)
+    buying_stock, selling_stock = res.split('$!$')
+
+    buying_stock = buying_stock.split('$$')
+    selling_stock = selling_stock.split('$$')
+
+    print(Fore.GREEN + 'BUYING:' + Fore.RESET)
+    for x in buying_stock:
+        print(x)
+
+    print(Fore.GREEN + 'SELLING:' + Fore.RESET)
+    for y in selling_stock:
+        print(y)
+
+    buying_stock.clear()
+    selling_stock.clear()
+    return
+
+
+def top_server(sk):
+    print(Fore.CYAN + 'TOP SERVER' + Fore.RESET)
+    send_req(sk, '2')
+    res = recv_res(sk)
+    top_legit, top_spent = res.split('$!$')
+
+    top_legit = top_legit.split('$$')
+    top_spent = top_spent.split('$$')
+
+    print(Fore.GREEN + 'Top transaction trader' + Fore.RESET)
+    for a in reversed(top_legit):
+        print(a)
+
+    print(Fore.GREEN + 'Top spent trader' + Fore.RESET)
+    for b in reversed(top_spent):
+        print(b)
+
+    top_legit.clear()
+    top_spent.clear()
     return
 
 
--- Server.py
+++ Server.py
@@ -2,6 +2,24 @@ import socket
 import pymongo
 
 
+DB_client = pymongo.MongoClient('mongodb://localhost:27017')
+database = DB_client['Trading_Bot']
+stock = database['Stock_4_Trade']
+sell_orders = database['Sell_Order']
+buy_orders = database['Buy_Order']
+account = database['User']
+success_trade = database['Successful_Trade']
+
+lst_user = []
+lst_Stock = []
+for x in stock.find({}, {"_id": 0}):
+    lst_Stock.append(x)
+
+for user in account.find({}, {"_id": 0}):
+    lst_user.append(user)
+
+
+
 def create_socket(h, p):
     sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Socket created')
@@ -41,22 +59,84 @@ def start_server(sk):
 
 
 def process_client_request(client_sk):
+    process_login(client_sk)
     while True:
         req = recv_req(client_sk)
         print('Client request: {}'.format(req))
+
         if req == 'Exit':
             break
+
+        menu(client_sk, req)
     return
 
 
-def menu(req):
-    match req:
-        case 1:
-            all_item()
-        case 2:
-            top_server()
-        case 3:
-            buy_item()
+def process_login(client_sk):
+    req = recv_req(client_sk)
+    username, password = req.split(' ')
+    count = 0
+    for x in lst_user:
+        if x['name'] == username and x['password'] == password:
+            count += 1
+    if count > 0:
+        send_res(client_sk, 'success')
+        print('Login successful !')
+        return
+    send_res(client_sk, 'fail')
+    print('Login fail !')
+    return
+
+
+def menu(client_sk, req):
+    if req == '1':
+        all_item(client_sk)
+
+    if req == '2':
+        top_server(client_sk)
+
+    # if req == '3':
+    #     search_item(client_sk)
+    #
+    # if req == '4':
+    #     buy_item(client_sk)
+    #
+    # if req == '5':
+    #     sell_item(client_sk)
+    #
+    # if req == '6':
+    #     transaction_history(client_sk)
+    #
+    # if req == '7':
+    #     disconnect(client_sk)
+
+
+def all_item(client_sk):
+    buying = ''
+    selling = ''
+    for x in reversed(lst_Stock):
+        if x['status'] == 'WTB':
+            buying = str(x) + '$$' + buying
+
+        if x['status'] == 'WTS':
+            selling = str(x) + '$$' + selling
+
+    all_stock = buying + '$!$' + selling
+    send_res(client_sk, all_stock)
+
+
+def top_server(client_sk):
+    top_legit = ''
+    top_spent = ''
+
+    for a in account.find().sort('legit_point', -1).limit(3):
+        top_legit = top_legit + '$$' + str([a['name'], a['legit_point']])
+
+    for b in account.find().sort('money_spent', -1).limit(3):
+        top_spent = top_spent + '$$' + str([b['name'], b['money_spent']])
+
+    dash_board = top_legit + '$!$' + top_spent
+
+    send_res(client_sk, dash_board)
 
 
 if __name__ == '__main__':
